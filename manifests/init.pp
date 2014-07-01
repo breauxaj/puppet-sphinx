09688922dff7e6e59a93c98872703160d4d931ce
Adam Breaux <breauxaj@gmail.com>
2014-07-01 13:15:49 -0400

    package does not create user or  group?

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,6 +5,33 @@ class sphinx (
     /(?i-mx:centos|fedora|redhat|scientific)/ => 'sphinx',
   }
 
+  $paths = $::operatingsystem ? {
+    /(?i-mx:centos|fedora|redhat|scientific)/ => [
+      '/var/log/sphinx'
+    ],
+  }
+
   package { $required: ensure => $ensure }
+
+  group { 'sphinx':
+    ensure => present,
+    gid    => 9312,
+  }
+
+  user { 'sphinx':
+    ensure     => present,
+    gid        => 9312,
+    home       => '/var/lib/sphinx',
+    shell      => '/sbin/nologin',
+    managehome => true,
+    uid        => 9312,
+  }
+
+  file { $paths:
+    ensure => directory,
+    owner  => 'sphinx',
+    group  => 'sphinx',
+    mode   => '0755',
+  }
 
 }
